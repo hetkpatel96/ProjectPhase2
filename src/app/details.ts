9b2e63af1937d34060d278d2a2e15c421779e6a5
Het Patel <hp428@njit.edu>
2020-10-18 07:57:26 -0500

    Added the service files and created the questions object

---
+++ src/app/details.ts
@@ -0,0 +1,6 @@
+
+export class Details{
+    question: string;
+    option: string[];
+    answer: string;
+}
---
+++ src/app/questions.ts
@@ -0,0 +1,2 @@
+
+export const detail : any = JSON.parse('[{"question": "What is the capital of India ?", "options": ["Delhi", "Mumbai"], "answer": "Delhi"}]')
